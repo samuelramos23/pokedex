3dca4162ee4d869ad5cd07be43c8694564ded655
Samuel <samuel@okn.com.br>
2022-01-26 14:00:01 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/common/repositories/pokemon_repository.dart';
+import 'package:pokedex/features/pokedex/route.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex 2K21',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: PokedexRoute(
+        repository: PokemonRepository(
+          dio: Dio(),
+        ),
+      ),
+    );
+  }
+}
